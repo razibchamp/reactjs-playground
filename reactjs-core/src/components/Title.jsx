84bb50789c84edd47e06aa9b42d4ad1582b3a7f2
Razib Shuvo <razib.shuvo>
2023-08-30 20:40:36 +0600

    component did mount, using state

--- reactjs-core/src/components/Title.jsx
+++ reactjs-core/src/components/Title.jsx
@@ -3,12 +3,30 @@ import React from 'react';
 export default class Title extends React.Component {
 
     state = {
-        title : this.props.title
+        date : new Date()
+    }
+
+    componentDidMount() {
+        this.clockTimer = setInterval(() => {
+            this.setState(() => {
+                return {
+                    date : new Date()
+                }
+            })
+        }, 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.clockTimer);
     }
     
     render() {
-        console.log('re-rendering from titleComponent')
-        const {title} = this.state
-        return <h1>This is a title updated {this.props.title}</h1>
+        const {title} = this.props
+        return (
+            <>
+                <h1>This is a title updated {title}</h1>
+                <h2>Current time : {this.state.date.toLocaleTimeString()}</h2>
+            </>
+        )
     }
 }
