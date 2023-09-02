98d30925fa4c45734ff9863ed2899800462ef8d3
razibchamp <razibhossain.shuvo@gmail.com>
2023-09-02 09:41:28 +0600

    handled name change using controlled form

--- reactjs-core/src/components/Title.jsx
+++ reactjs-core/src/components/Title.jsx
@@ -8,14 +8,15 @@ export default class Title extends React.Component {
 
         this.state = {
             lastName : props.lastName,
-            fullName : props.title
+            fullName : props.title,
+            fullNameInput: ''
         }
     }
 
     shouldComponentUpdate(nextProps, nextState) {
-        const { fullName : currentFullName } = nextState
-        const { fullName : prevFullName } = this.state
-        if ( currentFullName === prevFullName ) {
+        const { fullName : currentFullName, fullNameInput} = nextState
+        const { fullName : prevFullName, fullNameInput : prevFullNameInput } = this.state
+        if ( currentFullName === prevFullName && fullNameInput === prevFullNameInput) {
             return false;
         }
 
@@ -30,9 +31,17 @@ export default class Title extends React.Component {
             }
         })
     }
+
+    saveName = (e) => {
+        e.preventDefault()
+        this.setState({
+            fullName: this.state.fullNameInput,
+            lastName: ''
+        })
+    }
     
     render() {
-        const {fullName} = this.state
+        const {fullName, fullNameInput} = this.state
         const {gender} = this.props
         const honorific = gender === 'male' ? 'Mr. ' : 'Mrs. '
         console.log('re-rendering title')
@@ -41,6 +50,14 @@ export default class Title extends React.Component {
                 <h1>Hello {honorific + fullName}</h1>
                 <Clock />
                 <button onClick={this.changeName}>Show Full Name</button>
+                <form onSubmit={this.saveName}>
+                    <input type='text' name='fullNameInput'  value={fullNameInput} onChange={ (e) => {
+                        this.setState({
+                            [e.target.name]: e.target.value
+                        })
+                    }}/>
+                    <input type='submit' value="save name"/>
+                </form>
             </>
         )
     }
